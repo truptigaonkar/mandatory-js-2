835b2b1be12030c3bee6128bc9bb66c807fbf3f8
truptigaonkar <trups11.trupti@gmail.com>
2018-11-29 13:37:24 +0100

    Reset button functionality added by using only 'location.reload();'

--- tictactoe.js
+++ tictactoe.js
@@ -1,108 +1,112 @@
-
 var xMoves = [];
 var oMoves = [];
 var isOver = false; // see whether game is ended
 
 $(document).ready(function () {
-  var player = "x";
+    var player = "x";
 
-  $("td").on("click", function () {
+    // Reset button functionality
+    $("button").on("click", function () {
+        location.reload();
+    });
 
-    // Function so that player can not do any more moves once win the game
-    if (isOver) {
-        return;
-      } 
+    $("td").on("click", function () {
 
-    var marked = $(this);
-
-    if (marked.hasClass("x") || marked.hasClass("o")) {
-      alert("Please choose another square!")
-    } else {
-      if (player === "x") {
-        $("#message").text("It's X's turn!")
-        marked.addClass("x");
-        xMoves.push(this.id);
-        console.log(xMoves);
-        if (checkDiag(diagArr(3, 1), xMoves) || checkDiag(diagArr(3, 0), xMoves) || checkOther(xMoves)) {
-            $("#message").text("Player X wins!") // if either one of 3 winning conditions meet,
-            isOver = true; // game is ended
-        } else {
-          player = "o";
-          $("#message").text("It's O's turn!")
-
+        // Function so that player can not do any more moves once win the game
+        if (isOver) {
+            return;
         }
-      } else {
 
-        marked.addClass("o");
-        oMoves.push(this.id);
-        if (checkDiag(diagArr(3, 1), oMoves) || checkDiag(diagArr(3, 0), oMoves) || checkOther(oMoves)) {
-            $("#message").text("Player O wins!")
-          isOver = true;
+        var marked = $(this);
+
+        if (marked.hasClass("x") || marked.hasClass("o")) {
+            alert("Please choose another square!")
         } else {
-          player = "x";
-          $("#message").text("It's X's turn!")
+            if (player === "x") {
+                $("#message").text("It's X's turn!")
+                marked.addClass("x");
+                xMoves.push(this.id);
+                console.log(xMoves);
+                if (checkDiag(diagArr(3, 1), xMoves) || checkDiag(diagArr(3, 0), xMoves) || checkOther(xMoves)) {
+                    $("#message").text("Player X wins!") // if either one of 3 winning conditions meet,
+                    isOver = true; // game is ended
+                } else {
+                    player = "o";
+                    $("#message").text("It's O's turn!")
+
+                }
+            } else {
+
+                marked.addClass("o");
+                oMoves.push(this.id);
+                if (checkDiag(diagArr(3, 1), oMoves) || checkDiag(diagArr(3, 0), oMoves) || checkOther(oMoves)) {
+                    $("#message").text("Player O wins!")
+                    isOver = true;
+                } else {
+                    player = "x";
+                    $("#message").text("It's X's turn!")
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  var diagArr = function (size, booleanNum) {
-    var row = [];
-    var col = [];
-    var diagonal = [];
+    var diagArr = function (size, booleanNum) {
+        var row = [];
+        var col = [];
+        var diagonal = [];
 
-    for (var i = 1; i <= size; i++) {
-      i = String(i);
-      row.push(i);
+        for (var i = 1; i <= size; i++) {
+            i = String(i);
+            row.push(i);
 
-      if (booleanNum) {
-        col.unshift(i);
-      } else {
-        col.push(i);
-      }
-    }
+            if (booleanNum) {
+                col.unshift(i);
+            } else {
+                col.push(i);
+            }
+        }
 
-    for (var i = 0; i < row.length; i++) {
-      diagonal.push(row[i] + col[i]);
-    }
-    return diagonal;
-  };
+        for (var i = 0; i < row.length; i++) {
+            diagonal.push(row[i] + col[i]);
+        }
+        return diagonal;
+    };
 
-  var checkDiag = function (diagonal, playerMoves) {
+    var checkDiag = function (diagonal, playerMoves) {
 
-    for (var i = 0; i < diagonal.length; i++) {
-      if (playerMoves.indexOf(diagonal[i]) === -1) {
+        for (var i = 0; i < diagonal.length; i++) {
+            if (playerMoves.indexOf(diagonal[i]) === -1) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    var checkOther = function (playerMoves) {
+        var row = [];
+        var col = [];
+
+        for (var i = 0; i < playerMoves.length; i++) {
+            row.push(Number(playerMoves[i][0]));
+            col.push(Number(playerMoves[i][1]));
+        }
+
+        row.sort();
+        col.sort();
+
+        for (var i = 0; i < row.length; i++) {
+            if (row[i] === row[i + 1] && row[i] === row[i + 2]) {
+                return true;
+            }
+        }
+
+        for (var i = 0; i < col.length; i++) {
+            if (col[i] === col[i + 1] && col[i] === col[i + 2]) {
+                return true;
+            }
+        }
+
         return false;
-      }
-    }
-    return true;
-  };
-
-  var checkOther = function (playerMoves) {
-    var row = [];
-    var col = [];
-
-    for (var i = 0; i < playerMoves.length; i++) {
-      row.push(Number(playerMoves[i][0]));
-      col.push(Number(playerMoves[i][1]));
-    }
-
-    row.sort();
-    col.sort();
-
-    for (var i = 0; i < row.length; i++) {
-      if (row[i] === row[i + 1] && row[i] === row[i + 2]) {
-        return true;
-      }
-    }
-
-    for (var i = 0; i < col.length; i++) {
-      if (col[i] === col[i + 1] && col[i] === col[i + 2]) {
-        return true;
-      }
-    }
-
-    return false;
-  };
+    };
 
 });
